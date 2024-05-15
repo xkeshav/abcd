7f5e5e60e89b578e2aefed1ea51cefccc05f9669
Keshav Mohta <xkeshav@gmail.com>
2024-05-16 00:18:28 +0530

    added fonts and about page link on home page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from 'astro/config';
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    devToolbar: {enabled: false}
+  devToolbar: { enabled: false },
+  integrations: [sitemap(), partytown()],
+  image: {
+    domains: []
+  }
 });
---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+export type SiteConfig = {
+  website: string;
+  author: string;
+  title: string;
+  description: string;
+  language: string;
+  date: any;
+};
